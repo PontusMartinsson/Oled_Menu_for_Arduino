5d9276f0c00541e0338b591010d1db5164f9ae68
PontusMartinsson <pontus.martinsson@elev.ga.ntig.se>
2024-02-15 20:06:21 +0100

    Add comment block to src files

--- src/PotusOledMenu.h
+++ src/PotusOledMenu.h
@@ -1,3 +1,37 @@
+/*
+  PotusOledMenu.h
+
+  C++ Arduino wrapper for the PotusOledMenu struct and c functions for the PotusOledMenu library
+
+  Author: Pontus Martinsson
+  Created: 2024-01-30
+  Last modified: 2024-02-15
+
+  For more information, visit: https://github.com/Pontana22/PotusOledMenu
+
+  License: MIT License
+
+  Copyright (c) 2024 Pontus Martinsson
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 #ifndef PotusOledMenu_h
 #define PotusOledMenu_h
 
